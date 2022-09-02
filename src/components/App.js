5cbd0cad5b7aaa672961698240391117f9fa4e20
vaishnavi-gupta18 <vaishnavi_g@me.iitr.ac.in>
2022-09-03 01:01:40 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,8 @@
 import React from 'react'
-import { Link, HashRouter as Router, Routes, Route, useParams } from "react-router-dom";
 import '../styles/App.css';
 
-const EmployeeDetails = () => {
-  //Complete this function
-}
-
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route exact path="/:id/:name/:designation" element={<EmployeeDetails />} />
-      </Routes>
-    </Router>
-  )
+  //Write your code here
 }
 
 export default App;
